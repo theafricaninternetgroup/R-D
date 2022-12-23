54e1970ce97244cacad69b886fb0472f9f1dcd14
Dennis Mwape <dennis.mwape@africaninternetgroup.com>
2022-12-23 22:45:04 +0200

    Add simple multitenancy functionality and razor pages sample

---
+++ Multitenancy/Multitenancy.Library/ITenantResolver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multitenancy.Library
+{
+    public interface ITenantResolver<TTenant>
+    {
+       public TTenant? Tenant { get; }
+    }
+}
---
+++ Multitenancy/Samples/RazorPages/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Multitenancy.Web.Page
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
